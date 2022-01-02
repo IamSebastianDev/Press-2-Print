6abc03552e66ce41f4326cb5de77a0e911ba4b66
Sebastian <sebastian@iamsebastian.dev>
2022-01-02 22:55:06 +0100

    COMP: Added Menu component

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,49 @@
+/** @format */
+
+import classes from './Menu.module.css';
+
+import { PixelButton } from '../UI/PixelButton';
+import { Heading } from '../UI/Heading';
+import { List } from '../UI/List';
+
+export const Menu = ({ handleNavigation }) => (
+	<div style={{ margin: 'auto' }}>
+		<Heading
+			heading="Press to Print"
+			subheading="A Historically Accurate Typing Game"></Heading>
+		<List>
+			<li>
+				<PixelButton
+					handleOnClick={() => {
+						handleNavigation({ target: 1 });
+					}}>
+					Start new Game
+				</PixelButton>
+			</li>
+			<li>
+				<PixelButton
+					handleOnClick={() => {
+						handleNavigation({ target: 2 });
+					}}>
+					Options
+				</PixelButton>
+			</li>
+			<li>
+				<PixelButton
+					handleOnClick={() => {
+						handleNavigation({ target: 3 });
+					}}>
+					Highscores
+				</PixelButton>
+			</li>
+			<li>
+				<PixelButton
+					handleOnClick={() => {
+						handleNavigation({ target: 4 });
+					}}>
+					About
+				</PixelButton>
+			</li>
+		</List>
+	</div>
+);
