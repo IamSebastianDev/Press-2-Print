0cac63c19072d2b6805ab9749e217a99b228258f
Sebastian <sebastian@iamsebastian.dev>
2022-01-02 23:11:58 +0100

    FEAT: Added sorting to the Highscores

--- src/Components/Highscores/Highscores.js
+++ src/Components/Highscores/Highscores.js
@@ -1,24 +1,25 @@
 /** @format */
 
-import classes from './Highscores.module.css';
+// import classes from './Highscores.module.css';
 
 import { PixelButton } from '../UI/PixelButton';
 import { Heading } from '../UI/Heading';
 import { List } from '../UI/List';
+import { Score } from './Score';
 
 export const Highscores = () => {
 	let mockdata = [
 		{
-			date: Date.now(2000123),
-			points: 1440,
+			date: new Date(1233141231314),
+			score: 1440,
 		},
 		{
-			date: Date.now(12341233),
-			points: 140,
+			date: new Date(2312341231312),
+			score: 140,
 		},
 		{
-			date: Date.now(1233142),
-			points: 14120,
+			date: new Date(1312341265419),
+			score: 14120,
 		},
 	];
 
@@ -31,13 +32,15 @@ export const Highscores = () => {
 		<div>
 			<Heading heading="Highscores" />
 			<List>
-				{highscores.map(({ date, points }, index) => (
-					<li key={index}>
-						<PixelButton>
-							{new Date(date).toLocaleString()} {points}
-						</PixelButton>
-					</li>
-				))}
+				{highscores
+					.sort((a, b) => (a.score > b.score ? -1 : 1))
+					.map(({ date, score }, index) => (
+						<li key={index}>
+							<PixelButton>
+								<Score date={date} score={score} />
+							</PixelButton>
+						</li>
+					))}
 				{highscores.length === 0 && (
 					<li>
 						<PixelButton>No scores so far!</PixelButton>
