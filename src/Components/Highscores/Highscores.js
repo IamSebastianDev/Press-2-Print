c6c95943cc41cdcbcdfaa09c953dd7d017a1ab46
Sebastian <sebastian@iamsebastian.dev>
2022-01-03 16:45:15 +0100

    FIX: removed mock data

--- src/Components/Highscores/Highscores.js
+++ src/Components/Highscores/Highscores.js
@@ -8,23 +8,6 @@ import { List } from '../UI/List';
 import { Score } from './Score';
 
 export const Highscores = () => {
-	let mockdata = [
-		{
-			date: new Date(1233141231314),
-			score: 1440,
-		},
-		{
-			date: new Date(2312341231312),
-			score: 140,
-		},
-		{
-			date: new Date(1312341265419),
-			score: 14120,
-		},
-	];
-
-	window.localStorage.setItem('p2p-highscores', JSON.stringify(mockdata));
-
 	let highscores =
 		JSON.parse(window.localStorage.getItem('p2p-highscores')) || [];
 
@@ -33,7 +16,7 @@ export const Highscores = () => {
 			<Heading heading="Highscores" />
 			<List>
 				{highscores
-					.sort((a, b) => (a.score > b.score ? -1 : 1))
+					.sort((a, b) => (a.score >= b.score ? -1 : 1))
 					.map(({ date, score }, index) => (
 						<li key={index}>
 							<PixelButton>
