a7dedc63db7a01d8099345b93af0abb4d5d41899
Sebastian <sebastian@iamsebastian.dev>
2022-01-03 16:46:20 +0100

    PROG: Worked on Board component

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,9 +1,29 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './Board.module.css';
+import phrases from '../../assets/texts/volgate.js';
+
+import { Typetray } from './Typetray/Typetray';
+import { Printplate } from './Printplate/Printplate';
+import { Source } from './Source/Source';
 
 export const Board = () => {
-	return <div className={classes.board}></div>;
+	let [stage, setStage] = useState(10);
+	let [activeChar, setActiveChar] = useState(0);
+
+	let printed = [...phrases[stage]];
+
+	const handlePrint = () => {
+		console.log('Print');
+	};
+
+	return (
+		<div className={classes.board}>
+			<Typetray chars={[]} handlePrint={handlePrint} />
+			<Printplate printed={printed} />
+			<Source phrase={phrases[stage]} activeChar={activeChar} />
+		</div>
+	);
 };
