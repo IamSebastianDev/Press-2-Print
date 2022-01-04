8ae15b958fb0fe810e3e24cea01b2954d9c141da
Sebastian <sebastian@iamsebastian.dev>
2022-01-04 20:52:26 +0100

    PROG: Further work on Typetray

--- src/Components/Board/Typetray/Typetray.js
+++ src/Components/Board/Typetray/Typetray.js
@@ -4,15 +4,30 @@ import classes from './Typetray.module.css';
 import { Type } from './Type';
 import { PixelButton } from '../../UI/PixelButton';
 
-export const Typetray = ({ chars = [], handlePrint }) => {
+export const Typetray = ({
+	randomChars = [],
+	hasStarted,
+	roundFinished,
+	startGame,
+	nextRound,
+	addDieToPrint,
+}) => {
+	const button = [
+		<PixelButton handleOnClick={startGame}>Start game!</PixelButton>,
+		<PixelButton handleOnClick={nextRound}>Print line!</PixelButton>,
+	];
+
 	return (
 		<div className={classes.tray}>
-			{chars.map((char, index) => (
-				<Type key={index}>{char}</Type>
-			))}
-			{chars.length === 0 && (
-				<PixelButton handleOnClick={handlePrint}>Print!</PixelButton>
-			)}
+			{!hasStarted && button[0]}
+			{hasStarted && roundFinished && button[1]}
+			{hasStarted &&
+				!roundFinished &&
+				randomChars.map((char, index) => (
+					<Type addDieToPrint={addDieToPrint} key={index}>
+						{char.toLowerCase()}
+					</Type>
+				))}
 		</div>
 	);
 };
