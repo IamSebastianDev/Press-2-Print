78620d89de83ecaacc309d349bd51d62ce12dbb1
Sebastian <sebastian@iamsebastian.dev>
2022-01-05 22:46:20 +0100

    FEAT: Added audio toggle to navigation

--- src/Components/Navigation/Navgiation.js
+++ src/Components/Navigation/Navgiation.js
@@ -1,16 +1,37 @@
 /** @format */
 
-import classes from './Navigation.module.css';
+import React, { useState, useContext } from 'react';
 
 import { ChevronLeft } from 'react-pangolicons';
+import { VolumeMute, VolumeHigh } from 'react-pangolicons';
+
+import { SettingsContext } from '../../store/settings-context';
+
+import classes from './Navigation.module.css';
 
 export const Navigation = ({ mode, handleReturnToMenu }) => {
+	const ctx = useContext(SettingsContext);
+
 	return (
 		<nav className={classes.nav}>
+			{mode !== 0 && (
+				<button
+					className={classes.nav__action_button}
+					onClick={handleReturnToMenu}>
+					<ChevronLeft strokeWidth={3}></ChevronLeft>
+				</button>
+			)}
 			<button
+				style={{ marginLeft: 'auto' }}
 				className={classes.nav__action_button}
-				onClick={handleReturnToMenu}>
-				<ChevronLeft strokeWidth={3}></ChevronLeft>
+				onClick={() => {
+					ctx.handleAudio(!ctx.audio);
+				}}>
+				{ctx.audio ? (
+					<VolumeHigh strokeWidth={2} />
+				) : (
+					<VolumeMute strokeWidth={2} />
+				)}
 			</button>
 		</nav>
 	);
