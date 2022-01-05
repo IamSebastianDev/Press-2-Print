6fe65176189c5b8081b6b110edbe5c54949ad44f
Sebastian <sebastian@iamsebastian.dev>
2022-01-05 22:45:10 +0100

    FEAT: Added sound to pixelbutton

--- src/Components/UI/PixelButton.js
+++ src/Components/UI/PixelButton.js
@@ -1,15 +1,39 @@
 /** @format */
 
-import classes from './PixelButton.module.css';
+import React, { useContext } from 'react';
+import { SettingsContext } from '../../store/settings-context';
 
+import useSound from 'use-sound';
+import hoverFx from '../../assets/sounds/onEnterHover.mp3';
+import clickFx from '../../assets/sounds/onClick.mp3';
 import { Circle } from 'react-pangolicons';
 
+import classes from './PixelButton.module.css';
+
 export const PixelButton = ({ handleOnClick = null, children }) => {
+	const settings = useContext(SettingsContext);
+
 	const attributes = {
 		strokeWidth: 5,
 		classNames: 'button__svg',
 		size: 18,
 	};
+
+	const onClick = () => {
+		handleOnClick();
+		playOnClick({ id: 'click' });
+	};
+
+	const [playOnHover] = useSound(hoverFx, {
+		playbackRate: 2,
+		soundEnabled: settings.audio,
+	});
+	const [playOnClick] = useSound(clickFx, {
+		sprite: {
+			click: [400, 500],
+		},
+		soundEnabled: settings.audio,
+	});
 
 	return (
 		<button
@@ -18,7 +42,9 @@ export const PixelButton = ({ handleOnClick = null, children }) => {
 				' ' +
 				(handleOnClick !== null ? classes.button__button : '')
 			}
-			onClick={handleOnClick}>
+			onFocus={playOnHover}
+			onClick={onClick}
+			onMouseEnter={playOnHover}>
 			<Circle {...attributes} />
 			<span className={classes.button__text}>{children}</span>
 			<Circle {...attributes} />
